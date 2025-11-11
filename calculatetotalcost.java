3176f5d0159ec310ccf97bf8af91b0208408d5bd
MWABILI DAVID <mwabilidavid5@gmail.com>
2025-11-11 18:43:30 +0300

    Update calculatetotalcost.java

--- calculatetotalcost.java
+++ calculatetotalcost.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 public class calculatetotalcost {
+    public static double calculatetotalcost(double accomodationcost, double mealcost, double activitycost){
+        return accomodationcost +mealcost +activitycost;
+    }
   public static void main(String[] args){
       Scanner scanner=new Scanner(System.in);
-      double accomdation;
-      double meals;
-      double activities;
 
       System.out.println("Enter accomodation cost: ");
-      accomdation= scanner.nextDouble();
+      double accomodationcost= scanner.nextDouble();
 
       System.out.println("Enter meals cost: ");
-      meals=scanner.nextDouble();
+      double mealcost=scanner.nextDouble();
 
       System.out.println("Enter activities cost:");
-      activities=scanner.nextDouble();
+      double activitycost=scanner.nextDouble();
 
       // calculate total cost
 
-      double totalcost=accomdation+meals+activities;
+      double totalcost = calculatetotalcost(accomodationcost,mealcost,activitycost);
 
-      System.out.println("total cost:"+totalcost);
+      System.out.println("Total vacation cost:$"+totalcost);
 
 
   }
