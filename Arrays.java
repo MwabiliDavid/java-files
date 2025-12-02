40898a08a9047505d909d6f210bc4a06c0be996c
MWABILI DAVID <mwabilidavid5@gmail.com>
2025-12-02 17:04:03 +0300

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+import javax.xml.transform.SourceLocator;
+import java.util.Scanner;
+ // Arrays -1D
+
+public class Arrays{
+    public static void main(String[] args){
+
+        int[] numbers = {5,10,15,20,25};
+        int sum = 0;
+
+        for(int i=0;i<numbers.length;i++){
+            System.out.println(numbers[i]);
+            sum +=numbers[i];
+        }
+        System.out.println( "The sum is:" + sum);
+    }
+}
