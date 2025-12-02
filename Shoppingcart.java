3fe68d482f8c53b8d28d03b1ded9c10835512e0c
MWABILI DAVID <mwabilidavid5@gmail.com>
2025-12-02 17:18:21 +0300

    Add files via upload

---
+++ Shoppingcart.java
@@ -0,0 +1,16 @@
+import javax.xml.transform.SourceLocator;
+import java.util.Scanner;
+public class Shoppingcart {
+    public static void main(String[]args){
+
+        int[] prices={120,80,150,60,200,90};
+
+        System.out.println("Item prices:");
+        int total = 0;
+        for(int i = 0; i < prices.length;i++){
+            System.out.println(prices[i]);
+            total +=prices[i];
+        }
+        System.out.println("Total amount is:"+ total);
+    }
+}
